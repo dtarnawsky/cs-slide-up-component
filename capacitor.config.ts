c000ec3401e672afaef9c67a90b5a283c71e11e3
Damian Tarnawsky <damian@ionic.io>
2023-03-01 10:37:27 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.slide.up',
+  appName: 'cs-slide-up',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
